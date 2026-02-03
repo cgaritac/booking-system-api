d16c404c884bacb407c4c3e8d4d2f023da058a4e
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-03 17:37:00 -0600

    Feat: Add BaseEntity

--- src/main/java/com/example/booking/common/entity/BaseEntity.java
+++ src/main/java/com/example/booking/common/entity/BaseEntity.java
@@ -1,5 +1,17 @@
 package com.example.booking.common.entity;
 
-public class BaseEntity {
-    
+import jakarta.persistence.*;
+import lombok.Getter;
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt = Instant.now();
 }
