39b1bcf1f84b117edb882e24f2e89235cfbb5d36
Carlos Garita Campos <cgaritac@gmail.com>
2026-02-03 18:35:23 -0600

    Feat: Add Availability model

---
+++ src/main/java/com/example/booking/availability/entty/AvailabilityEntity.java
@@ -0,0 +1,27 @@
+package com.example.booking.availability.entty;
+
+import com.example.booking.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "availability")
+public class AvailabilityEntity extends BaseEntity {
+    @Column(nullable = false)
+    private UUID staffId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DayOfWeek dayOfWeek;
+
+    @Column(nullable = false)
+    private LocalTime startTime;
+
+    @Column(nullable = false)
+    private LocalTime endTime;
+}
